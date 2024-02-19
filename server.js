49c47df94290885fa96e602489aa25d1a7a15eeb
Jay Yu <jy195@hotmail.com>
2024-02-19 07:49:58 -0500

    add all basic routes

--- server.js
+++ server.js
@@ -18,12 +18,80 @@ app.get('/', (req, res) => {
   res.json({msg: "Default loader"});
 })
 
+app.post('/api/companies', (req, res) => {
+  const newCompany = req.body;
 
+  db.addNewCompany(newCompany)
+    .then((newComp) => {
+      res.json(newComp);
+    })
+    .catch((err) => {
+      res.json({ error: err.message});
+    })
+})
+
+app.get('/api/companies', (req, res) => {
+  const page = parseInt(req.query.page);
+  const perPage = parseInt(req.query.perPage);
+  const name = req.query.name;
+
+  if(isNaN(page) || isNaN(perPage)) {
+    return res.status(400).json({
+      page: page,
+      perPage: perPage,
+      error: "Page and perPage query parameters must be valid numbers"
+    });
+  }
+
+  db.getAllCompanies(page, perPage, name)
+    .then((companies) => {
+      res.json(companies);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message})
+    });
+})
+
+app.get('/api/company/:id', (req, res) => {
+  const companyId = req.params.id;
+  db.getCompanyById(companyId)
+  .then((company) => {
+    res.json(company);
+  })
+  .catch((err) => {
+    res.status(500).json({ error: err.message})
+  });
+})
+
+app.put('/api/company/:id', (req, res) => {
+  const companyId = req.params.id;
+  const companyData = req.body;
+  console.log("PUT company by id")
+
+  db.updateCompanyById(companyData, companyId)
+    .then((company) => {
+      res.json(company);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message})
+    });
+})
+
+app.delete('/api/company/:id', (req, res) => {
+  const companyId = req.params.id;
+
+  db.deleteCompanyById(companyId)
+    .then((deleteResult) => {
+      res.json(deleteResult)
+    })
+    .catch((err) => {
+      res.json({ error: err.message})
+    });
+})
 
 db.initialize(process.env.MONGODB_CONN_STRING).then(() => {
   app.listen(HTTP_PORT, () => {
     console.log(`Server listening on: ${HTTP_PORT}`);
-    console.log(db)
   });
 }).catch((err) => {
   console.log("initialize() failed!");
